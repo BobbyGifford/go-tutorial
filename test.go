a871024d5c67182dff9a44f8d85cb6f21f3bef42
Bobby Gifford <bgifford@drishticon.net>
2018-04-05 14:12:39 -0700

    Testing values in strings

--- test.go
+++ test.go
@@ -6,7 +6,6 @@ var decksize int
 
 var test = 70
 
-
 func testFun(testVar int) (int, string) {
 	return (testVar * 2), "Go can have two return values"
 }
@@ -15,6 +14,10 @@ func main() {
 	// innerTest := "New var"
 	// fmt.Println(test)
 	// fmt.Println(innerTest)
+
+	testVar1 := "this is valid"
+
+	fmt.Printf("testVar is of type %T \n", testVar1)
 
 	fmt.Println(testFun(10))
 }
